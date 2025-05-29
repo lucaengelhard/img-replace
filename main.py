d54ce6f4fa7866e6394e679ca101a832c6c5d7f8
Luca Engelhard <me@lucaengelhard.com>
2025-05-29 14:20:35 +0200

    get path from argv

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from face_detection import detect_faces, frame_faces
-from tinyface import FacePair, TinyFace
-import cv2
+import sys
+import os
 
+import cv2
+from tinyface import TinyFace
+
+from face_detection import detect_faces
+from utils import frame_faces
+
+# Setup
 tinyface = TinyFace()
 tinyface.prepare()
 
-faces = detect_faces("./imgs/base_img.jpg")
-shifted_faces = [faces[-1]] + faces[:-1]
+# Arguments
+path = "./imgs/base_img.jpg"
+if len(sys.argv) > 1:
+    path = sys.argv[1]
 
-pairs = []
 
+img = cv2.imread(path)
+
+# tiny = tinyface.get_many_faces(img)
+
+faces = detect_faces(path)
+# shifted_faces = [faces[-1]] + faces[:-1]
 
 # output_img = tinyface.swap_faces(
-#     cv2.imread("./imgs/base_img.jpg"),
+#     img,
 #     face_pairs=[
-#         FacePair(reference=faces[i]["cutout"], destination=shifted_faces[i]["cutout"])
+#         FacePair(reference=faces[i], destination=shifted_faces[i])
 #         for i in range(len(faces))
 #     ],
 # )
+
+frame_faces(img, faces, True)
+
+cv2.imshow("res", img)
+cv2.waitKey(0)
